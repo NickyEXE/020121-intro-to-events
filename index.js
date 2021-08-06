9cb932decb249fb22eecedc431cea87d4f8af3b0
NickyEXE <dovern42@gmail.com>
2021-08-05 20:23:08 -0400

    end of lecture 2

--- index.js
+++ index.js
@@ -7,20 +7,45 @@ const comments = document.getElementById("comments")
 const initialEmojis = ["🍓", "🍕", "🥔", "😋", "🚽", "💪"]
 initialEmojis.forEach(addEmoji)
 
-document.querySelector('form').addEventListener("submit", handleSubmit)
+emojiForm.addEventListener("submit", handleSubmit)
+commentForm.addEventListener("submit", handleCommentSubmit)
 
 document.getElementById("taco").addEventListener("click", function(){addEmoji("🌮")})
 
-emojisContainer.addEventListener("click", () => console.log("clicked"))
+
+// anti-pattern: Does not apply to added emojis
+// document.querySelectorAll("li").forEach(element => {
+//   element.addEventListener("dblclick", (e) => e.target.remove())
+// })
 
 function addEmoji(emoji){
-  const emojiButton = document.createElement("li")
-  emojiButton.innerText = emoji
-  emojisContainer.appendChild(emojiButton)
+  emojisContainer.innerHTML += `<li data-emoji=${emoji}>'${emoji}'</li>`
 }
 
+emojisContainer.addEventListener("dblclick", (e) => {
+  if (e.target.tagName == "LI"){
+    if (e.target.dataset.emoji == "🌮"){
+      alert("YUM")
+    }
+    e.target.remove()
+  }
+})
+
+// closure method:
+// function addEmoji(emoji){
+//   const emojiButton = document.createElement("li")
+//   emojiButton.innerText = emoji
+//   emojisContainer.appendChild(emojiButton)
+//   // let counter = 0
+//   // emojiButton.addEventListener("click", increaseCounter)
+//   // function increaseCounter(){
+//   //   counter++
+//   //   console.log(`counter for ${emoji}:`, counter)
+//   // }
+//   emojiButton.addEventListener("dblclick", () => emojiButton.remove())
+// }
+
 document.querySelector("li").addEventListener("click", function(e){
-  e.stopPropagation()
   e.target.style.backgroundColor = "yellow"
 })
 
@@ -28,4 +53,37 @@ function handleSubmit(e){
   e.preventDefault()
   addEmoji(e.target.emoji.value)
   e.target.reset()
+}
+
+function handleCommentSubmit(e){
+  e.preventDefault()
+  // you can also use e.target to get commentForm
+  const initialClicks = commentForm.initialClicks.value
+  const comment = commentForm.comment.value
+  addComment(initialClicks, comment)
+  commentForm.reset()
+}
+
+// Event Delegation:
+
+// function addComment(initialClicks, comment){
+//   comments.innerHTML += `<p data-initial-clicks=${initialClicks} data-comment=${comment}>${initialClicks}: ${comment}</p>`
+// }
+
+// comments.addEventListener("click", function(e){
+//   if (e.target.tagName == "P"){
+//     ++e.target.dataset.initialClicks
+//     e.target.innerText = `${e.target.dataset.initialClicks}: ${e.target.dataset.comment}`
+//   }
+// })
+
+// Closures:
+
+function addComment(initialClicks, comment){
+  const newComment = document.createElement("p")
+  newComment.innerText = `${initialClicks}: ${comment}`
+  comments.appendChild(newComment)
+  newComment.addEventListener("click", function(){
+    newComment.innerText = `${++initialClicks}: ${comment}`
+  })
 }
